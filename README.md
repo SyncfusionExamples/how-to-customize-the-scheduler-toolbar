3986fe35af3a91dc00739b8b4df06a99e1afbf79
Balasubramanian Sattanathan <balasubramanian.sattanathan@syncfusion.com>
2020-01-28 18:07:26 +0530

    Committed project for customizing the scheduler toolbar

---
+++ README.md
@@ -0,0 +1,39 @@
+# Syncfusion Angular 8 Scheduler Project
+
+A project that helps you to avoid infinte loop on applying method in `workDays` property binding with code snippet.
+
+## Scheduler features covered in this Project
+
+This is an Angular 8 project created using [Angular CLI](https://github.com/angular/angular-cli) 8.3.20. The Scheduler features included in this project are as follows.
+* Angular 8 Scheduler displaying basic views with appointments loaded as Object.
+* Setting workDays through method
+
+## How to run this application?
+
+To run this application, you need to first clone the `how-to-prevent-infinite-loop-on-workDays-property-binding` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionSamples/how-to-prevent-infinite-loop-on-workDays-property-binding angular-8-scheduler
+cd angular-8-scheduler
+```
+
+## Installing
+Once done with downloading, next you need to install the necessary packages required to run this application locally. The `npm install` command will install all the needed angular packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  TimelineViewsService, ResizeService, DragAndDropService, EventSettingsModel,
+  ScheduleComponent, ActionEventArgs
+} from '@syncfusion/ej2-angular-schedule';
+import { scheduleData } from './data';
+import { extend } from '@syncfusion/ej2-base';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [TimelineViewsService, ResizeService, DragAndDropService]
+})
+export class AppComponent {
+  @ViewChild('schedule', null) scheduleObj: ScheduleComponent;
+  public data: object[] = extend([], scheduleData, null, true) as object[];
+  public selectedDate: Date = new Date(2018, 1, 15);
+  public views: object[] = [{ option: 'TimelineWeek', interval: 4 }];
+  public eventSettings: EventSettingsModel = { dataSource: this.data };
+  public flag = true;
+
+  onActionBegin(args: ActionEventArgs): void {
+    if (args.requestType === 'toolbarItemRendering') {
+      const arg = args as any;
+      const item0 = arg.items[0];
+      const item1 = arg.items[1];
+      item1.align = 'right';
+      // Getting date range text
+      let item2 = arg.items[2];
+      const dateRangeText = {
+        align: 'center',
+        text: item2.text,
+        cssClass: 'e-custom'
+      };
+      item2 = dateRangeText;
+      // Rearranging the toolbar items
+      arg.items[0] = item0;
+      arg.items[1] = item2;
+      arg.items[2] = item1;
+    }
+  }
+
+  // Updating date range text on initial loading
+  onDataBinding(): void {
+    if (this.flag) {
+      this.dateRangetext();
+      this.flag = false;
+    }
+  }
+
+  // Updating date range text on navigating
+  onActionComplete(args: ActionEventArgs): void {
+    if (args.requestType === 'dateNavigate') {
+      this.dateRangetext();
+    }
+  }
+
+
+  public dateRangetext(): void {
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const startDate = (this.scheduleObj.getCurrentViewDates()[0] as Date);
+    const endDate = this.scheduleObj.getCurrentViewDates()[this.scheduleObj.getCurrentViewDates().length - 1] as Date;
+    const startYear = startDate.getFullYear() === endDate.getFullYear() ? '' : ', ' + startDate.getFullYear();
+    const startTxt = '' + monthNames[startDate.getMonth()] + ' ' + startDate.getDate() + '' + startYear + ' -';
+    const endTxt = ' ' + monthNames[endDate.getMonth()] + ' ' + endDate.getDate() + ', ' + endDate.getFullYear();
+    (document.querySelector('.e-toolbar-item.e-custom') as HTMLElement).innerText = startTxt + endTxt;
+  }
+}
